a395fca3b0fc6bb19446f355028b0bd3f4fc88ce
Uche Emmanuel <urtchman04@yahoo.co.uk>
2024-12-13 16:52:51 -0500

    Uche created the component file Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+
+
+const Footer = () => (
+    <footer>
+      <div className="footer-content">
+        <div className="contact-info">
+          <address>
+            123 Travel Street, City, Country<br />
+            Phone: (123) 456-7890<br />
+            Email: info@umktravels.com
+          </address>
+        </div>
+        <div className="social-links">
+          <a href="#">Facebook</a>
+          <a href="#">Twitter</a>
+          <a href="#">Instagram</a>
+        </div>
+        <p>&copy; 2024 TravelAgent. All Rights Reserved.</p>
+      </div>
+    </footer>
+  );
+
+
+export default Footer;  
